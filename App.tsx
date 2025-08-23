7d52cb4d0a2a8d093de646d3a06a25e1598ba44b
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2025-08-23 12:05:12 +0300

    Initialize project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import Home from '@/Home';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsxxxxxx to start working on your app!</Text>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
